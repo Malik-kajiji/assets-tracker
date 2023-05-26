2605c69f0ab8bd9993086ba07aa40c28d976f913
Vishal <vishal.pugazhendhi@gmail.com>
2023-05-26 20:30:05 +0530

    Changes to date

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,13 @@ import '../styles/home.css';
 import { FiLogOut } from 'react-icons/fi';
 import { auth, db } from '../firebaseConfig';
 import { signOut } from 'firebase/auth';
-import { doc, getDoc, onSnapshot } from 'firebase/firestore';
+import { doc, getDoc, onSnapshot, setDoc } from 'firebase/firestore';
 import { AreaChart, XAxis, YAxis, Tooltip, Area } from 'recharts';
 import Asset from './Asset';
 import Header from './Header';
 import { LineChart } from './LineChart';
 import { PieChart } from './PieChart';
+import { AlertData } from '../context/AlertContext';
 
 const Home = () => {
     const [portfolio, setProtfolio] = useState({
@@ -18,43 +19,60 @@ const Home = () => {
         initialBalance: 0,
         startDate: ''
     })
-    const [pieChartData,setPieChartData] = useState({})
-    const [maxPercetage,setMaxPercentage] = useState(100);
+    const [pieChartData, setPieChartData] = useState({})
+    const [maxPercetage, setMaxPercentage] = useState(100);
     const [currentAsset, setCurrentAsset] = useState(null);
+    const { setAlertData } = AlertData()
 
     useEffect(() => {
+        console.log('useEffect');
         const Ref = doc(db, 'portfolio', auth.currentUser.uid)
         let removeSnapShot = onSnapshot(Ref, (res) => {
             if (res.exists()) {
                 let allAssestsPercentage = 0;
                 let newPrice = 0;
-                for(let i=0;i<res.data().assets.length;i++){
-                    allAssestsPercentage =+ res.data().assets[i].percentage
+                for (let i = 0; i < res.data().assets.length; i++) {
+                    allAssestsPercentage = + res.data().assets[i].percentage
                     newPrice += res.data().assets[i].recentPrice * res.data().assets[i].stocksAmount
                 }
                 setMaxPercentage(prev => prev - allAssestsPercentage)
-                let noneChangedBalance = res.data().initialBalance * ((100 - allAssestsPercentage)/100) 
+                let noneChangedBalance = res.data().initialBalance * ((100 - allAssestsPercentage) / 100)
                 let ChangedBalance = noneChangedBalance + newPrice
                 ChangedBalance = parseFloat(ChangedBalance.toFixed(2))
-                let growth = parseFloat((ChangedBalance/res.data().initialBalance).toFixed(2))
-                if(ChangedBalance === res.data().initialBalance){
+                let growth = parseFloat((ChangedBalance / res.data().initialBalance).toFixed(2))
+                if (ChangedBalance === res.data().initialBalance) {
                     growth = 0
                 }
-                setProtfolio({...res.data(),currentBalance:ChangedBalance,growth})
+                setProtfolio({ ...res.data(), currentBalance: ChangedBalance, growth })
             }
         })
         return () => removeSnapShot
-    }, [])
+    }, [portfolio.startDate])
 
 
-    useEffect(()=>{
+    useEffect(() => {
         let newObj = {}
-        for(let i =0; i< portfolio.assets.length ; i++){
+        for (let i = 0; i < portfolio.assets.length; i++) {
             newObj[portfolio.assets[i].asset] = portfolio.assets[i].percentage
         }
         setPieChartData(newObj)
-    },[portfolio.assets.length])
-    
+    }, [portfolio.assets.length])
+
+
+
+    const handleChange = e => {
+        if (new Date(e.target.value) < new Date('6/1/2022')) {
+            setAlertData({ type: 'warrning', showen: true, msg: "you can't choose date before this: (1/6/2022)" })
+        } else if (new Date(e.target.value) >= new Date(new Date().getTime() - (24 * 60 * 60 * 1000))) {
+            setAlertData({ type: 'warrning', showen: true, msg: "you can't choose a future date" })
+        } else {
+            setProtfolio(prev => ({ ...prev, startDate: e.target.value }))
+            const Ref = doc(db, 'portfolio', auth.currentUser.uid)
+            setDoc(Ref, { ...portfolio, startDate: e.target.value });
+            console.log(portfolio);
+        }
+    }
+
     return (
         <section>
             <Header startDate={portfolio.startDate} maxPercetage={maxPercetage} initiatBalance={portfolio.initialBalance} assets={portfolio.assets} />
@@ -63,7 +81,17 @@ const Home = () => {
                 <div className='Portfolio-status'>
                     <p>
                         <span className='title'>Start Date</span>
-                        <span className='data'>{portfolio.startDate}</span>
+                        <span className='data'>
+                            {/* {portfolio.startDate} */}
+                            <input
+                                type="date"
+                                autoComplete='off'
+                                id='startDate'
+                                name='startDate'
+                                value={portfolio.startDate}
+                                onChange={handleChange}
+                            />
+                        </span>
                     </p>
                     <p>
                         <span className='title'>Initial Balance</span>
@@ -75,31 +103,31 @@ const Home = () => {
                     </p>
                     <p>
                         <span className='title'>Growth</span>
-                        <span className={`data ${portfolio.growth>0? 'green' :'red'}`}>
+                        <span className={`data ${portfolio.growth > 0 ? 'green' : 'red'}`}>
                             {portfolio.growth > 0 ?
                                 `+${portfolio.growth}%`
-                            :
+                                :
                                 `${portfolio.growth}%`
                             }
                         </span>
                     </p>
                 </div>
                 <div className='portfolio-chart'>
-                    <PieChart piData={pieChartData}/>
+                    <PieChart piData={pieChartData} />
                 </div>
             </article>
             <ul>
-                {portfolio.assets.map((e,i)=>(
+                {portfolio.assets.map((e, i) => (
 
-                    <Asset 
-                        title={e.asset} 
-                        currentAsset={currentAsset} 
-                        setCurrentAsset={setCurrentAsset} 
-                        index={i} 
+                    <Asset
+                        title={e.asset}
+                        currentAsset={currentAsset}
+                        setCurrentAsset={setCurrentAsset}
+                        index={i}
                         key={i}
-                        startDate={portfolio.startDate} 
-                        balance={portfolio.initialBalance} 
-                        percentage={e.percentage} 
+                        startDate={portfolio.startDate}
+                        balance={portfolio.initialBalance}
+                        percentage={e.percentage}
                         stocksAmount={e.stocksAmount}
                     />
                 ))
@@ -113,4 +141,4 @@ const Home = () => {
     )
 }
 
-export default Home
+export default Home;
